d05fdc2f600de433a9718c126850c2fdbf39f51e
Yeshvanth Muniraj <Yeshvanth.Muniraj@analog.com>
2022-12-15 11:17:43 +0530

    Correct test project name and add project for stm32f429 target

--- tests/driver/gpio/stm32f429/RTE/_Simulator/RTE_Components.h
+++ tests/driver/gpio/stm32f429/RTE/_Simulator/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'driver_gpio_test_stm32f401' 
+ * Project: 'driver_gpio_test_stm32f429' 
  * Target:  'Simulator' 
  */
 
